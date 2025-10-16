1f4e9313f21a1edecd42b503aa01e0d701f8bfad
jenn-liuu <jl1391@duke.edu>
2025-10-15 22:25:00 -0400

    practiced with a new button in App

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,16 @@
+function MyButton() {
+  return (
+    <button>I'm a button</button>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <h1>Hello Vite + React!</h1>
+      <MyButton />
+    </div>
+  );
+}
+
+export default App;
