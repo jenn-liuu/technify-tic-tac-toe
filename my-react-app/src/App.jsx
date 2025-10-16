8e6c1ea8894436df0f374aa73f713f80a9c9cbdd
jenn-liuu <jl1391@duke.edu>
2025-10-16 15:28:27 -0400

    created square function

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,14 +1,3 @@
-function MyButton() {
-  return (
-    <button>I'm a button</button>
-  );
-}
-
-export default function MyApp() {
-  return (
-    <div>
-      <h1>Welcome to my app</h1>
-      <MyButton />
-    </div>
-  );
+export default function Square() {
+  return <button className="square">X</button>; 
 }
