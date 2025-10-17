ed6fe4818cbbad40cd5fef90e24e64db6262e36a
jenn-liuu <jl1391@duke.edu>
2025-10-17 15:38:32 -0400

    moved constant to Board

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,18 +1,11 @@
 import { useState } from 'react';
 
-function Square() {
-  const [squares, setSquares] = useState(Array(9).fill(null)); 
-
-  function handleClick() {
-    setValue('X'); 
-  }
-
-  return (
-    <button className="square">{value}</button> 
-  )
+function Square({ value }) {
+  return <button className="square">{value}</button>
 }
 
 export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null)); 
   return (
     <>
       <div className="board-row">
