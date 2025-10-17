200e39a06c37fb4b59cfbfa23d40dfff95a44e0b
jenn-liuu <jl1391@duke.edu>
2025-10-17 15:15:48 -0400

    imported useState to return values

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,4 +1,8 @@
-function Square({ value }) {
+import { useState } from 'react';
+
+function Square() {
+  const [value, setValue] = useState(null); 
+
   function handleClick() {
     console.log('clicked!'); 
   }
@@ -16,19 +20,19 @@ export default function Board() {
   return (
     <>
       <div className="board-row">
-        <Square value ="1" />
-        <Square value ="2" />
-        <Square value ="3" />
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value ="4" />
-        <Square value ="5" />
-        <Square value ="6" />
+        <Square value />
+        <Square value />
+        <Square value />
       </div>
       <div className="board-row">
-        <Square value ="7" />
-        <Square value ="8" />
-        <Square value ="9" />
+        <Square value />
+        <Square value />
+        <Square value />
       </div>
     </>
   ); 
