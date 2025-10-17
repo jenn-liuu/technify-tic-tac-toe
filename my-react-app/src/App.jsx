fb21ccb9fe7566d75a36663163acaf74e3d460b3
jenn-liuu <jl1391@duke.edu>
2025-10-17 15:36:46 -0400

    set array and values to squares

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,19 +1,14 @@
 import { useState } from 'react';
 
 function Square() {
-  const [value, setValue] = useState(null); 
+  const [squares, setSquares] = useState(Array(9).fill(null)); 
 
   function handleClick() {
     setValue('X'); 
   }
-  
+
   return (
-    <button 
-      className="square"
-      onClick={handleClick}
-    >
-      {value}
-    </button> 
+    <button className="square">{value}</button> 
   )
 }
 
@@ -21,19 +16,19 @@ export default function Board() {
   return (
     <>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} />
+        <Square value={squares[1]}/>
+        <Square value={square[2]}/>
       </div>
       <div className="board-row">
-        <Square value />
-        <Square value />
-        <Square value />
+        <Square value={square[3]}/>
+        <Square value={square[4]}/>
+        <Square value={square[5]}/>
       </div>
       <div className="board-row">
-        <Square value />
-        <Square value />
-        <Square value />
+        <Square value={square[6]}/>
+        <Square value={square[7]}/>
+        <Square value={square[8]}/>
       </div>
     </>
   ); 
