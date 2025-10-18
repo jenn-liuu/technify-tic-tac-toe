41bd161f4a5c37c72e87d28bf441b18888b5aa7e
jenn-liuu <jl1391@duke.edu>
2025-10-18 17:12:57 -0400

    made a component folder

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'; 
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'; 
-import Home from './Home'; 
-import Board from './Board'; 
+import Home from './components/Home'; 
+import Board from './components/Board'; 
 
 export default function App() {
   return (
