80a6bc6aa3ff785aa986582e4f38df709029c406
jenn-liuu <jl1391@duke.edu>
2025-10-18 16:39:08 -0400

    attempted to fix header

--- my-react-app/src/Header.jsx
+++ my-react-app/src/Header.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 export default function Header() {
     return (
-        <header className="header">
+        <header>
             <h1>Tic Tac Toe Game</h1> 
-            <p class="subtitle">Jennifer Liu</p>
+            <p className="subtitle">Jennifer Liu</p>
         </header>
     )
 }
